9889d2aa5bbd903c6237784ef3e661e5a4ac084a
David Stone <dcstone09@gmail.com>
2025-07-22 16:09:41 -0700

    ♻️ refactor: simplify config.js to use utils for binary path resolution
    - Replace hardcoded './apollo-mcp-server' path with dynamic getMCPServerPath()
    - Import getMCPServerPath from utils.js for consistent binary detection
    - Remove duplicated binary management logic from config
    - Improve separation of concerns between configuration and utility functions

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+const { getMCPServerPath } = require('./utils');
+
 module.exports = {
   SCHEMA_URL: 'https://staging-api.gethealthie.com/graphql',
   SCHEMA_DIR: './schemas',
   SCHEMA_FILE: './schemas/healthie-schema.graphql',
-  MCP_SERVER_PATH: './apollo-mcp-server'
+  MCP_SERVER_PATH: getMCPServerPath()
 };
